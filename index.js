567226a69c5fe15e7ca7514174b54d7a101e3444
david-chua <dp_chua@hotmail.com>
2019-03-07 20:59:35 -0500

    added port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const db = require('./data/db.js');
 
 const server = express();
-const PORT = '9090'
+const PORT = process.env.PORT || 9090
 
 server.use(express.json())
 
